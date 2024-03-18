69f72e8193bfb093942d70f79101a2cd33f53f9b
Андрей Романов <77008804+PICKLEGENT@users.noreply.github.com>
2024-03-18 11:29:39 +0300

    Api.js

---
+++ src/Api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers:
+  {
+    'X-RapidAPI-Key': YOUR_X-RapidAPI-Key,
+    'X-RapidAPI-Host': YOUR_X-RapidAPI-Host
+  }
+};
+
+export const GEO_API_URL = YOUR_GEO_API_URL;
+
+export const WEATHER_API_URL = YOUR_OPEN_WEATHER_API_URL;
+export const WEATHER_API_KEY = YOUR_OPEN_CAGE_API_KEY;
