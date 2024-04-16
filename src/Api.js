05bddd8bac2f0888f6968503ca6397406bc0325c
Андрей Романов <human1488pwnz@yandex.ru>
2024-04-16 22:21:38 +0300

    Revert "Merge branch 'main' of https://github.com/PICKLEGENT/Weather-App-2.0"
    This reverts commit cf82b0b5d37a524fda62717838f90557002b4322, reversing
    changes made to a7567017f12c9bfee9e4e90e7f838310303a61f8.

--- src/Api.js
+++ src/Api.js
@@ -5,15 +5,9 @@ export const geoApiOptions = {
 		'X-RapidAPI-Key': YOUR_X - RapidAPI - Key,
 		'X-RapidAPI-Host': YOUR_X - RapidAPI - Host
 	}
-	method: 'GET',
-	headers:
-	{
-		'X-RapidAPI-Key': YOUR_X - RapidAPI - Key,
-		'X-RapidAPI-Host': YOUR_X - RapidAPI - Host
-	}
 };
 
 export const GEO_API_URL = YOUR_GEO_API_URL;
 
-export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = '3adcfaba76e08011bd8ff507e1118964';
+export const WEATHER_API_URL = YOUR_OPEN_WEATHER_API_URL;
+export const WEATHER_API_KEY = YOUR_OPEN_CAGE_API_KEY;
